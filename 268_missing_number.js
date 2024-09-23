574d6ece5af9322b2f7f81e53c676214dccb7f4d
Laxmikantadas123 <daslaxmikanta711@gmail.com>
2024-09-23 13:05:08 +0530

    268_missing_number

---
+++ 268_missing_number.js
@@ -0,0 +1,10 @@
+//----------------------268_missing_number---------------------
+var missingNumber = function(nums) {
+    let res = nums.length;
+         
+         for (let i = 0; i < nums.length; i++) {
+             res += i - nums[i];
+         }
+         
+         return res;
+ };
